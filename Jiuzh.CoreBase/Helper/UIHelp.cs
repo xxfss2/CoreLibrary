4ee2c15619f828179d769baee8720d48af527f16
xxf <xxfss2@163.com>
2013-01-21 20:47:26 +0800

    update
    Helper 功能更新

--- Jiuzh.CoreBase/Helper/UIHelp.cs
+++ Jiuzh.CoreBase/Helper/UIHelp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace Jiuzh.CoreBase
 {
@@ -11,6 +12,11 @@ namespace Jiuzh.CoreBase
         public static object Bind<IView>(this Page page, Func<IView, object> func)
         {
             return func((IView)page.GetDataItem());
+        }
+
+        public static object Bind<IView>(this RepeaterItem item, Func<IView, object> func)
+        {
+            return func((IView)item.DataItem);
         }
     }
 }
